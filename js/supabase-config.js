c87d6ab59d941728ee78b0a8b130cf006f895a87
Matias Melo <140745565+GGubb@users.noreply.github.com>
2025-11-07 00:01:26 -0300

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const SUPABASE_URL = "https://xtgzlzkiydsxciblbgqw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0Z3psemtpeWRzeGNpYmxiZ3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2MzI3MzEsImV4cCI6MjA3NTIwODczMX0.xb6KZhtwxw4TS0QpBlwJSUp04278mD_JoF_odcZey48";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-window.supabase = supabase;
